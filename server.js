1f3dce3c0385561a7c4255ae2af5c94ee8df5cb5
rahul-katonic <118425532+rahul-katonic@users.noreply.github.com>
2025-09-15 19:33:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8050;
+const PORT = 8888;
 
 // Subpath to serve the app on
-const SUBPATH = process.env.ROUTE || '/app';
-app.get('/readyz', (req, res) => {
-  res.status(200).send('OK');
-});
-// Serve static files under the subpath
 app.use(express.static(path.join(__dirname, 'build')));
 
 // For all other routes under subpath, serve index.html (for SPA routing)
-app.get(`${SUBPATH}*`, (req, res) => {
+const SUBPATH = `/ws-ea5b4463-dd5d-4abc-88c5-875a4a0f3065-68c2d368257c4071a3a51116/8888/`;
+
+app.get(`/*`, (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
